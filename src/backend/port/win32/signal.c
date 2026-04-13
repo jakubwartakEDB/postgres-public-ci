0a23e36b41fdccc6f1ecbaf2887656098282c35e
Jakub Wartak <jakub.wartak@enterprisedb.com>
2026-04-13 13:24:41 +0200

    naive_test

--- src/backend/port/win32/signal.c
+++ src/backend/port/win32/signal.c
@@ -85,10 +85,10 @@ pgwin32_signal_initialize(void)
 
 	for (i = 0; i < PG_SIGNAL_COUNT; i++)
 	{
-		pg_signal_array[i].sa_handler = SIG_DFL;
+		pg_signal_array[i].sa_handler = PG_SIG_DFL;
 		pg_signal_array[i].sa_mask = 0;
 		pg_signal_array[i].sa_flags = 0;
-		pg_signal_defaults[i] = SIG_IGN;
+		pg_signal_defaults[i] = PG_SIG_IGN;
 	}
 	pg_signal_mask = 0;
 	pg_signal_queue = 0;
@@ -136,13 +136,16 @@ pgwin32_dispatch_queued_signals(void)
 				struct sigaction *act = &pg_signal_array[i];
 				pqsigfunc	sig = act->sa_handler;
 
-				if (sig == SIG_DFL)
+				if (sig == PG_SIG_DFL)
 					sig = pg_signal_defaults[i];
 				pg_signal_queue &= ~sigmask(i);
-				if (sig != SIG_ERR && sig != SIG_IGN && sig != SIG_DFL)
+				if (sig != SIG_ERR && sig != PG_SIG_IGN && sig != PG_SIG_DFL)
 				{
 					sigset_t	block_mask;
 					sigset_t	save_mask;
+					struct pg_signal_info nodata;
+					nodata.pid = 0;
+					nodata.uid = 0;
 
 					LeaveCriticalSection(&pg_signal_crit_sec);
 
@@ -151,7 +154,7 @@ pgwin32_dispatch_queued_signals(void)
 						block_mask |= sigmask(i);
 
 					sigprocmask(SIG_BLOCK, &block_mask, &save_mask);
-					sig(i);
+					sig(i, &nodata);
 					sigprocmask(SIG_SETMASK, &save_mask, NULL);
 
 					EnterCriticalSection(&pg_signal_crit_sec);
